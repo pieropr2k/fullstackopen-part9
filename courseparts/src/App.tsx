207d195567bc42a6d1355fc59e3ede59f5925c99
Piero <piero.pilco.r@uni.pe>
2022-11-29 00:50:26 -0500

    FSO Part9 Ex14

---
+++ courseparts/src/App.tsx
@@ -0,0 +1,73 @@
+interface HeaderProps {
+  name: string;
+}
+
+// Header component
+const Header = ({ name }: HeaderProps) => {
+  return (
+    <h2>{name}</h2>
+  )
+}
+
+interface CourseProps {
+  name: string;
+  exerciseCount: number;
+}
+
+interface Courses {
+  courseParts: CourseProps[]
+}
+
+const Content = ({ courseParts }: Courses) => {
+  return (
+    <>
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+    </>
+  )
+}
+
+// Total component
+const Total = ({ courseParts }: Courses) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+    </p>
+  )
+}
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content courseParts={courseParts} />
+      <Total courseParts={courseParts} />
+    </div>
+  );
+};
+
+export default App;
