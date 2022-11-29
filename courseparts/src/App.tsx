92835ccde3f6a122718ea706f9fac0963155e2d5
Piero <piero.pilco.r@uni.pe>
2022-11-29 18:56:51 -0500

    FSO Part9 Ex15

--- courseparts/src/App.tsx
+++ courseparts/src/App.tsx
@@ -1,3 +1,87 @@
+// new types
+// Exercise 9.15
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CourseNormalPart extends CoursePartBase {
+  type: "normal";
+  description: string;
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBase {
+  type: "submission";
+  description: string;
+  exerciseSubmissionLink: string;
+}
+
+interface CourseSpecialPart extends CoursePartBase {
+  type: "special";
+  description: string;
+  requirements: string[];
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
+
+interface SwitchComponentProps {
+  type: string;
+  description?: string;
+  groupProjectCount?: number;
+  exerciseSubmissionLink?: string;
+  requirements?: string[];
+}
+
+const SwitchComponent = ({type, description, groupProjectCount, exerciseSubmissionLink, requirements}: SwitchComponentProps) => {
+  switch (type) {
+    case "normal":
+      return (<i>{description}</i>)
+    case "groupProject":
+      return (<div>{`project exercises ${groupProjectCount}`}</div>)
+    case "submission":
+      return (<>
+          <i>{description}</i>
+          <div>{`submit to ${exerciseSubmissionLink}`}</div>
+        </>
+      )
+    case "special":
+      return (<>
+        <i>{description}</i>
+        {requirements ? <div>{`required skills: ${requirements.join(', ')}`}</div> : null}
+      </>)
+    default:
+      return (<div>{description}</div>);
+  }
+}
+
+interface CourseProps extends SwitchComponentProps{
+  name: string;
+  exerciseCount: number;
+}
+// Content component
+const Part = ({name, exerciseCount, type, description, groupProjectCount, exerciseSubmissionLink, requirements}: CourseProps) => {
+  return (
+    <>
+      <h4>
+        {name} {exerciseCount}
+      </h4>
+      <SwitchComponent 
+        type={type}
+        description={description}
+        groupProjectCount={groupProjectCount}
+        exerciseSubmissionLink={exerciseSubmissionLink}
+        requirements={requirements}
+      />
+    </>
+  )
+}
+
 interface HeaderProps {
   name: string;
 }
@@ -9,11 +93,6 @@ const Header = ({ name }: HeaderProps) => {
   )
 }
 
-interface CourseProps {
-  name: string;
-  exerciseCount: number;
-}
-
 interface Courses {
   courseParts: CourseProps[]
 }
@@ -21,15 +100,17 @@ interface Courses {
 const Content = ({ courseParts }: Courses) => {
   return (
     <>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
+      {courseParts.map(part => 
+        <Part 
+          key={part.name} 
+          name={part.name} 
+          exerciseCount={part.exerciseCount}
+          type={part.type}
+          description={part.description}
+          groupProjectCount={part.groupProjectCount}
+          exerciseSubmissionLink={part.exerciseSubmissionLink}
+          requirements={part.requirements}
+        />)}
     </>
   )
 }
@@ -46,20 +127,41 @@ const Total = ({ courseParts }: Courses) => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  // this is the new coursePart variable
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the easy course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the hard course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special"
     }
-  ];
+  ]
 
   return (
     <div>
